8f9a048dd80d411b6444b65675fc0a1673963e35
weston-bailey <wdbailey87@gmail.com>
2021-07-21 15:26:11 -0700

    update md

---
+++ BinaryTree.py
@@ -0,0 +1,183 @@
+# A node in a Binary Tree has data, left, and right properties
+# The left and right properties are intialized as null
+class Node:
+  def __init__(self, data):
+    self.left = None
+    self.right = None
+    self.data = data
+  
+  # the representaion of the data as a string
+  def __str__(self):
+    return f'{self.data}'
+
+
+class BinaryTree:
+  # When a new Tree is initialized, it has a null root property
+  # This means the Tree begins as empty
+  def __init__(self):
+    self.root = None
+
+  def insert(self, data):
+    '''
+      Insert(data: any) -> None: 
+      creates a new Node from the data passed in and adds it to the tree
+      If the data is already in the tree, does not insert it again
+    '''
+
+    # create new node
+    new_node = Node(data)
+
+    # if there is no root node, set new node to root node
+    if not self.root:
+      self.root = new_node
+      return
+
+    # loop over tree starting at the root
+    current_node = self.root
+    while current_node:
+      # if the data is smaller than left
+      if data < current_node.data:
+        # ...and there is no left
+        if not current_node.left:
+          # ...set data to to left node 
+          current_node.left = new_node
+          return
+        # ...otherwise set current_node to left node
+        else: current_node = current_node.left
+      # if the data is bigger than the right node
+      elif data > current_node.data:
+        # ...and there is no right node
+        if not current_node.right:
+          # ...the data becomes the right node
+          current_node.right = new_node
+          return
+        # ...otherwise move to the right 
+        else: current_node = current_node.right
+
+  def search(self, val):
+    '''
+      search(value: any) -> value or bool: 
+      Performs depth first search
+      Search the Tree for a node with the given value
+      If the node exists, return it
+      If the node doesn't exist, return false
+    '''
+
+    # case where there is not root yet
+    if not self.root:
+      return False
+
+    # loop through tree starting at the root
+    current_node = self.root
+    while current_node:
+      # if the value is smaller than the 
+      if val < current_node.data:
+        current_node = current_node.left
+      elif val > current_node.data:
+        current_node = current_node.right
+      else: 
+        return current_node
+
+    return False #  Outside loop means we did not find it!
+
+  # print out all the nodes
+  def print(self, node=None):
+    '''
+      print(node=optional: Node) -> None:
+      prints out all values
+      defualt start is at root node
+    '''
+
+    if not node:
+      node = self.root
+    
+    # first print the current node
+    print(node)
+    # recursively invoke if another node is found
+    if node.left:
+      self.print(node.left)
+    if node.right:
+      self.print(node.right)
+
+  # breadth first search
+  # Calculate the number of nodes in the tree, starting from the given node
+  # If no node is provided, we can use the root as a sensible default
+  def size(self, node=None):
+    '''
+      size(node=optional: Node) -> int: 
+      performs breadth first search
+      Calculate the number of nodes in the tree, starting from the given node
+      If no node is provided, we can use the root as a sensible default
+    '''
+    # defualt to root
+    if not node:
+      node = self.root
+
+    count = 0
+    def recursive_size(node):
+      nonlocal count
+      if node:
+        count += 1
+        recursive_size(node.left)
+        recursive_size(node.right)
+    recursive_size(node)
+    return count
+
+  # Return the maximum data value stored in the tree
+  def getMax(self):
+    '''
+      getMax() -> int: 
+      perform depth first search
+      Calculate the maximum value held in the tree
+    '''
+
+    if not self.root:
+      return None
+
+    current_node = self.root
+    while current_node.right:
+      current_node = current_node.right
+
+    return current_node.data
+
+  def getMin(self):
+    '''
+      getMin() -> int: 
+      perform depth first search
+      Calculate the minimum value held in the tree
+    '''
+
+    if not self.root: return None
+
+    current_node = self.root
+    while current_node.left:
+      current_node = current_node.left
+    
+    return current_node.data
+
+  # Calculate the maximum amount of nodes in any one path from the given node
+  # If not given a specific node, default to using the root node
+  def height(self, node=None):
+    '''
+      height(node=optional: Node) -> int: 
+      perform breadth first search
+      Calculate the maximum amount of nodes in any one path from the given node
+      If not given a specific node, default to using the root node
+    '''
+
+    # defualt to root
+    if not node:
+      node = self.root
+
+    max_height = 0
+    def recursive_height(node, height=1):
+      nonlocal max_height
+      if node:
+        if height > max_height:
+          max_height = height
+
+        recursive_height(node.left, height + 1)
+        recursive_height(node.right, height + 1)
+
+    recursive_height(node)
+    return max_height
---
+++ README.md
@@ -0,0 +1,37 @@
+#  Binary Search Tree 🌴
+
+We are going to be implementing a Binary Tree together! 🎉
+
+I like to think of a Binary Tree as a pre-sorted linked list. There is a little alorithmic logic to how a BST stores the data.
+
+To get started head over to BinaryTree.py and check out the classes there. There are method stubs that we are going to fill out together!
+
+## Links
+
+gitbook:
+
+https://gawdiseattle.gitbook.io/wdi/08-cs/cs-trees-data-structures
+
+imbalanced binary tree:
+
+https://appliedgo.net/balancedtree/
+
+visualgo:
+
+https://visualgo.net/en/bst
+
+code along built from theses resources:
+
+https://www.tutorialspoint.com/python_data_structure/python_binary_tree.html
+
+https://www.geeksforgeeks.org/binary-tree-set-1-introduction/
+
+## Here is a visual representaion of a Binary Search Tree
+
+![BST](imgs/BST.png)
+
+## Here is a visual representation of a Trie
+
+![Trie](imgs/Trie.png)
+
+###### tags: `lessons`
