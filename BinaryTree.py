402e8ea68d2cfe845274265c89d267ebac11e4b5
weston-bailey <wdbailey87@gmail.com>
2022-07-07 14:14:53 -0700

    format

--- BinaryTree.py
+++ BinaryTree.py
@@ -18,9 +18,9 @@ class BinaryTree:
 
     def insert(self, data):
         '''
-			Insert(data: any) -> None:\n 
-			creates a new Node from the data passed in and adds it to the tree
-			If the data is already in the tree, does not insert it again
+        Insert(data: any) -> None:\n 
+        creates a new Node from the data passed in and adds it to the tree
+        If the data is already in the tree, does not insert it again
         '''
 
         # create new node
@@ -60,11 +60,11 @@ class BinaryTree:
 
     def search(self, val):
         '''
-			search(value: any) -> value or bool:\n 
-			Performs depth first search
-			Search the Tree for a node with the given value
-			If the node exists, return it
-			If the node doesn't exist, return false
+        search(value: any) -> value or bool:\n 
+        Performs depth first search
+        Search the Tree for a node with the given value
+        If the node exists, return it
+        If the node doesn't exist, return false
         '''
 
         # case where there is not root yet
@@ -87,9 +87,9 @@ class BinaryTree:
     # print out all the nodes
     def print(self, node=None):
         '''
-			print(node=optional: Node) -> None:\n
-			prints out all values recursively (in a breadth first search fashion)
-			defualt start is at root node
+        print(node=optional: Node) -> None:\n
+        prints out all values recursively (in a breadth first search fashion)
+        defualt start is at root node
         '''
 
         if not node:
@@ -108,10 +108,10 @@ class BinaryTree:
     # If no node is provided, we can use the root as a sensible default
     def size(self, node=None):
         '''
-			size(node=optional: Node) -> int:\n 
-			performs breadth first search
-			Calculate the number of nodes in the tree, starting from the given node
-			If no node is provided, we can use the root as a sensible default
+        size(node=optional: Node) -> int:\n 
+        performs breadth first search
+        Calculate the number of nodes in the tree, starting from the given node
+        If no node is provided, we can use the root as a sensible default
         '''
         # defualt to root
         if not node:
@@ -132,10 +132,10 @@ class BinaryTree:
     # If not given a specific node, default to using the root node
     def height(self, node=None):
         '''
-			height(node=optional: Node) -> int:\n 
-			perform breadth first search
-			Calculate the maximum amount of nodes in any one path from the given node
-			If not given a specific node, default to using the root node
+        height(node=optional: Node) -> int:\n 
+        perform breadth first search
+        Calculate the maximum amount of nodes in any one path from the given node
+        If not given a specific node, default to using the root node
         '''
 
         # defualt to root
@@ -159,9 +159,9 @@ class BinaryTree:
     # Return the maximum data value stored in the tree
     def get_max(self):
         '''
-			getMax() -> int:\n 
-			perform depth first search
-			Calculate the maximum value held in the tree
+        getMax() -> int:\n 
+        perform depth first search
+        Calculate the maximum value held in the tree
         '''
 
         if not self.root:
@@ -175,9 +175,9 @@ class BinaryTree:
 
     def get_min(self):
         '''
-			getMin() -> int:\n 
-			perform depth first search
-			Calculate the minimum value held in the tree
+        getMin() -> int:\n 
+        perform depth first search
+        Calculate the minimum value held in the tree
         '''
 
         if not self.root:
